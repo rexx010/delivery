eb89e7b3eab3694933835c8cffa5ce1a4c76dab0
rexx010 <grexx200@gmail.com>
2025-09-08 16:39:14 +0100

    created item model in the delivery app

--- src/main/java/com/newProject/data/models/Item.java
+++ src/main/java/com/newProject/data/models/Item.java
@@ -1,4 +1,22 @@
 package com.newProject.data.models;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
 public class Item {
+    @Id
+    private String id;
+    private String itemName;
+    private String itemDescription;
+    private int weightInGrams;
+    private String senderName;
+    private String receiverName;
+    private String contactPhoneNumber;
+
+    @DBRef
+    private TrackingInfosImp trackingInfos;
 }
